23fed8a3f0db5c407023e991e49a67910f6d79c9
LeonardPasquier <leonard.pasquier@gmail.com>
2016-12-30 16:04:08 +0100

    On rajoute la fenetre du choix des projets, accessible depuis mon entreprise

--- app/src/main/java/corporation/unpitch/idlegame/ChoixProjet.java
+++ app/src/main/java/corporation/unpitch/idlegame/ChoixProjet.java
@@ -1,7 +1,9 @@
 package corporation.unpitch.idlegame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -19,7 +21,23 @@ public class ChoixProjet extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choixprojetihm);
 
+        accept_projet1 = (Button)findViewById(R.id.acc_projet1);
+        accept_projet2 = (Button)findViewById(R.id.acc_projet2);
+        accept_projet3 = (Button)findViewById(R.id.acc_projet3);
+
+        accept_projet1.setOnClickListener(accepterProjet);
+        accept_projet2.setOnClickListener(accepterProjet);
+        accept_projet3.setOnClickListener(accepterProjet);
+
     }
+    private View.OnClickListener accepterProjet = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent inventaire = new Intent (ChoixProjet.this, MainActivity.class); // on declare la nouvelle activite reliee au bouton
+            startActivity (inventaire); //on demarre l'activite
+        }
+
+    };
     @Override
     public void onBackPressed(){
         this.finish();
