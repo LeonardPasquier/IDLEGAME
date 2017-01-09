f922ef937b2e734738bf9a790d99a9c93d198efa
LeonardPasquier <leonard.pasquier@gmail.com>
2017-01-09 18:00:56 +0100

    Le compteur permettant l'incrémentation automatique du code est fonctionnel.

--- app/src/main/java/corporation/unpitch/idlegame/Donnees.java
+++ app/src/main/java/corporation/unpitch/idlegame/Donnees.java
@@ -8,33 +8,36 @@ import java.io.Serializable;
 
 class Donnees implements Serializable {
     //On déclare les variables à sauvegarder
-    private String lignes_de_code_courantes ="null";
-    private String lignes_de_code_total ="null";
-    private String argent = "null";
-    private String argent_total="null";
+    private int lignes_de_code_courantes =0;
+    private int lignes_de_code_total =0;
+    private int argent = 0;
+    private int argent_total=0;
     private String projet_courant_general ="rien";
     private String projet_courant_general_id = "";
-    private String projet_courant_facile = "facile1";
-    private String projet_courant_moyen ="moyen1";
-    private String projet_courant_difficile ="difficile1";
-    private String nombre_dev_j="null";
-    private String nombre_dev_e="null";
-    private String nombre_dev_s="null";
+    private String projet_courant_jeux_video = "jeux_video1";
+    private String projet_courant_site_web ="site_web1";
+    private String projet_courant_logiciel ="logiciel1";
+    private int nombre_dev_j= 1;
+    private int nombre_dev_e=1;
+    private int nombre_dev_s=1;
+    private int nombre_chef_projet_j= 1;
+    private int nombre_chef_projet_e=1;
+    private int nombre_chef_projet_s=1;
 
     //On fait les setter pour leur affecter une valeur depuis une autre classe
-    void setLignes_de_code_courantes(String lignes_de_code_courantes){
+    void setLignes_de_code_courantes(int lignes_de_code_courantes){
         this.lignes_de_code_courantes = lignes_de_code_courantes;
     }
 
-    void setLignes_de_code_total(String lignes_de_code_total){
+    void setLignes_de_code_total(int lignes_de_code_total){
         this.lignes_de_code_total = lignes_de_code_total;
     }
 
-    void setArgent(String argent){
+    void setArgent(int argent){
         this.argent = argent;
     }
 
-    void setArgent_total(String argent_total){
+    void setArgent_total(int argent_total){
         this.argent_total = argent_total;
     }
 
@@ -42,45 +45,60 @@ class Donnees implements Serializable {
         this.projet_courant_general = projet_courant_general;
     }
 
-    void setProjet_courant_facile(String projet_courant_facile){
-        this.projet_courant_facile = projet_courant_facile;
+    void setProjet_courant_jeux_video(String projet_courant_jeux_video){
+        this.projet_courant_jeux_video = projet_courant_jeux_video;
     }
 
-    void setProjet_courant_moyen(String projet_courant_moyen){
-        this.projet_courant_moyen = projet_courant_moyen;
+    void setProjet_courant_site_web(String projet_courant_site_web){
+        this.projet_courant_site_web = projet_courant_site_web;
     }
 
-    void setProjet_courant_difficile(String projet_courant_difficile){
-        this.projet_courant_difficile = projet_courant_difficile;
+    void setProjet_courant_logiciel(String projet_courant_logiciel){
+        this.projet_courant_logiciel = projet_courant_logiciel;
     }
 
-    void setNombre_dev_j(String nombre_dev_j){
+    void setNombre_dev_j(int nombre_dev_j){
         this.nombre_dev_j = nombre_dev_j;
     }
 
-    void setNombre_dev_e(String nombre_dev_e){
+    void setNombre_dev_e(int nombre_dev_e){
         this.nombre_dev_e = nombre_dev_e;
     }
 
-    void setNombre_dev_s(String nombre_dev_s){
+    void setNombre_dev_s(int nombre_dev_s){
         this.nombre_dev_s = nombre_dev_s;
     }
+    
+    void setNombre_chef_projet_j(int nombre_chef_projet_j){
+        this.nombre_chef_projet_j = nombre_chef_projet_j;
+    }
+
+    void setNombre_chef_projet_e(int nombre_chef_projet_e){
+        this.nombre_chef_projet_e = nombre_chef_projet_e;
+    }
+
+    void setNombre_chef_projet_s(int nombre_chef_projet_s){
+        this.nombre_chef_projet_s = nombre_chef_projet_s;
+    }
+    
     void setProjet_courant_general_id (String projet_courant_general_id){this.projet_courant_general_id=projet_courant_general_id;}
+    
+    
 
     //Puis les getter pour récupérer leur valeur depuis une autre classe
-    String getLignes_de_code_courantes(){
+    int getLignes_de_code_courantes(){
         return this.lignes_de_code_courantes;
     }
 
-    String getLignes_de_code_total(){
+    int getLignes_de_code_total(){
         return this.lignes_de_code_total;
     }
 
-    String getArgent(){
+    int getArgent(){
         return this.argent;
     }
 
-    String getArgent_total(){
+    int getArgent_total(){
         return this.argent_total;
     }
 
@@ -88,28 +106,41 @@ class Donnees implements Serializable {
         return this.projet_courant_general;
     }
 
-    String getProjet_courant_facile(){
-        return this.projet_courant_facile;
+    String getProjet_courant_jeux_video(){
+        return this.projet_courant_jeux_video;
     }
 
-    String getProjet_courant_moyen(){
-        return this.projet_courant_moyen;
+    String getProjet_courant_site_web(){
+        return this.projet_courant_site_web;
     }
 
-    String getProjet_courant_difficile(){
-        return this.projet_courant_difficile;
+    String getProjet_courant_logiciel(){
+        return this.projet_courant_logiciel;
     }
 
-    String getNombre_dev_j(){
+    int getNombre_dev_j(){
         return this.nombre_dev_j;
     }
 
-    String getNombre_dev_e(){
+    int getNombre_dev_e(){
         return this.nombre_dev_e;
     }
 
-    String getNombre_dev_s(){
+    int getNombre_dev_s(){
         return this.nombre_dev_s;
     }
+    
+    int getNombre_chef_projet_j(){
+        return this.nombre_chef_projet_j;
+    }
+
+    int getNombre_chef_projet_e(){
+        return this.nombre_chef_projet_e;
+    }
+
+    int getNombre_chef_projet_s(){
+        return this.nombre_chef_projet_s;
+    }
+    
     String getProjet_courant_general_id(){ return this.projet_courant_general_id;}
 }
