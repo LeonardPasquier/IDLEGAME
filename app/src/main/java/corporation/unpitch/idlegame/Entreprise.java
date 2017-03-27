d0a2ef2f3b188ab6f4f9469e7ef73b347556224a
Tbreizh <guillotristan24@gmail.com>
2017-03-27 13:43:43 +0200

    graphisme onglet entreprise et choix projet (manque juste bouton choisir)

--- app/src/main/java/corporation/unpitch/idlegame/Entreprise.java
+++ app/src/main/java/corporation/unpitch/idlegame/Entreprise.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Un Pitch on 03/11/2016.
@@ -17,10 +18,17 @@ public class Entreprise extends AppCompatActivity{
     Button inventaire = null;
     Button choix_projet = null;
 
+    TextView nomEntreprise ;
+    TextView nomCreateur ;
+    TextView argentTotale;
+    TextView LigneDeCodeTotales;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.entrepriseihm);
+
 
         recruter = (Button)findViewById(R.id.recruter);
         entreprise = (Button)findViewById(R.id.entreprise);
@@ -31,6 +39,21 @@ public class Entreprise extends AppCompatActivity{
         entreprise.setOnClickListener(lienEntreprise);
         inventaire.setOnClickListener(lienInventaire);
         choix_projet.setOnClickListener(lienChoixProjet);
+
+        nomEntreprise = (TextView) findViewById(R.id.textViewNomEntreprise);
+        nomEntreprise.setText ((" Entreprise : \n ")+(String.valueOf(MainActivity.donnees.getNom_entreprise())));
+
+        nomCreateur = (TextView) findViewById(R.id.textViewNomCreateur);
+        nomCreateur.setText((" Créateur : ")+String.valueOf(MainActivity.donnees.getNom_joueur()));
+
+        argentTotale = (TextView) findViewById(R.id.textViewArgentTotale);
+        argentTotale.setText((" Argent total : ")+String.valueOf(MainActivity.donnees.getArgent_total())+("€"));
+
+        LigneDeCodeTotales = (TextView) findViewById(R.id.textViewLignesDeCodesTotales);
+        LigneDeCodeTotales.setText((" Lignes de codes totales : ")+String.valueOf(MainActivity.donnees.getLignes_de_code_total()));
+
+
+
     }
 
     private View.OnClickListener lienRecrutement = new View.OnClickListener() {
